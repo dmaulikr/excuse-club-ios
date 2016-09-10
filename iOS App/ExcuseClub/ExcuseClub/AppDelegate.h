7a35a7a3667ddee20976a8996f93612676142513
Marian Goia <mariangoia91@gmail.com>
2016-09-10 15:10:10 -0700

    iOS App Initial Setup

---
+++ iOS App/ExcuseClub/ExcuseClub/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ExcuseClub
+//
+//  Created by Marian Goia on 10/09/16.
+//  Copyright © 2016 Marian Goia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ iOS App/ExcuseClub/ExcuseClub/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ExcuseClub
+//
+//  Created by Marian Goia on 10/09/16.
+//  Copyright © 2016 Marian Goia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
