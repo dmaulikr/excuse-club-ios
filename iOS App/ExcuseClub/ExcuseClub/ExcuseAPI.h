a93ce4f3e46e41fd3565a824c1426fa1503a5ee7
pinglamb <pinglambs@gmail.com>
2016-09-10 22:41:17 -0700

    Happy buddies API

--- iOS App/ExcuseClub/ExcuseClub/ExcuseAPI.h
+++ iOS App/ExcuseClub/ExcuseClub/ExcuseAPI.h
@@ -13,6 +13,9 @@
 
 + (ExcuseAPI *)client;
 
-- (void)registerUserWithName:(NSString *)name andPhone:(NSString *)phone;
+- (void)registerUserWithName:(NSString *)name andPhone:(NSString *)phone success:(void (^)(NSDictionary *user))success failure:(void (^)(NSString *error))failure;
+- (void)getBuddies:(void (^)(NSArray *buddies))success failure:(void (^)(NSString *error))failure;
+- (void)createBuddies:(NSArray *)buddies success:(void (^)(NSArray *buddies))success failure:(void (^)(NSString *error))failure;
+
 
 @end
--- iOS App/ExcuseClub/ExcuseClub/ExcuseAPI.m
+++ iOS App/ExcuseClub/ExcuseClub/ExcuseAPI.m
@@ -27,15 +27,74 @@ NSString *secret;
     return self;
 }
 
-- (void)registerUserWithName:(NSString *)name andPhone:(NSString *)phone {
+- (void)registerUserWithName:(NSString *)name andPhone:(NSString *)phone success:(void (^)(NSDictionary *user))success failure:(void (^)(NSString *error))failure {
     [manager POST:[self urlWithPath:@"/api/users.json"]
        parameters:[self parametersWithBody:@{@"name" : name, @"phone" : phone}]
          progress:nil
           success:^(NSURLSessionTask *task, id responseObject) {
-        NSLog(@"JSON: %@", responseObject);
-    } failure:^(NSURLSessionTask *operation, NSError *error) {
-        NSLog(@"Error: %@", error);
-    }];
+              NSLog(@"JSON: %@", responseObject);
+              if ([responseObject objectForKey:@"error"]) {
+                  if(failure) {
+                      failure([responseObject objectForKey:@"error"]);
+                  }
+              } else {
+                  if(success) {
+                      success([responseObject objectForKey:@"user"]);
+                  }
+              }
+          } failure:^(NSURLSessionTask *operation, NSError *error) {
+              NSLog(@"Error: %@", error);
+              if(failure) {
+                  failure([error localizedDescription]);
+              }
+          }];
+}
+
+- (void)createBuddies:(NSArray *)buddies success:(void (^)(NSArray *))success failure:(void (^)(NSString *))failure {
+    [manager POST:[self urlWithPath:@"/api/buddies.json"]
+       parameters:[self parametersWithBody:@{@"buddies" : buddies}]
+         progress:nil
+          success:^(NSURLSessionTask *task, id responseObject) {
+           NSLog(@"JSON: %@", responseObject);
+           if ([responseObject objectForKey:@"error"]) {
+               if(failure) {
+                   failure([responseObject objectForKey:@"error"]);
+               }
+           } else {
+               if(success) {
+                   success([responseObject objectForKey:@"buddies"]);
+               }
+           }
+       } failure:^(NSURLSessionTask *operation, NSError *error) {
+           NSLog(@"Error: %@", error);
+           if(failure) {
+               failure([error localizedDescription]);
+           }
+       }];
+}
+
+- (void)getBuddies:(void (^)(NSArray *))success failure:(void (^)(NSString *))failure
+{
+    [manager GET:[self urlWithPath:@"/api/buddies.json"]
+       parameters:[self parametersWithBody:@{}]
+         progress:nil
+         success:^(NSURLSessionTask *task, id responseObject) {
+             NSLog(@"JSON: %@", responseObject);
+             if ([responseObject objectForKey:@"error"]) {
+                 if(failure) {
+                     failure([responseObject objectForKey:@"error"]);
+                 }
+             } else {
+                 if(success) {
+                     success([responseObject objectForKey:@"buddies"]);
+                 }
+             }
+         } failure:^(NSURLSessionTask *operation, NSError *error) {
+             NSLog(@"Error: %@", error);
+             if(failure) {
+                 failure([error localizedDescription]);
+             }
+         }];
 }
 
 - (NSString *)urlWithPath:(NSString *)path {
@@ -47,6 +106,8 @@ NSString *secret;
 - (NSDictionary *)parametersWithBody:(NSDictionary *)body {
     NSMutableDictionary *params = [@{} mutableCopy];
     [params setObject:secret forKey:@"api_key"];
+    // FIXME
+    [params setObject:@"QVY3axZjBfwGChv6Zu8pc1r6" forKey:@"token"];
     [params addEntriesFromDictionary:body];
     return params;
 }
